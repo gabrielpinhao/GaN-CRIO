05f407322cf5247970afb3d802eadca4e9b6f920
Mateus Caruso <mateuscaruso@gmail.com>
2026-04-16 19:14:50 -0300

    .

--- ClassDATA.py
+++ ClassDATA.py
@@ -254,7 +254,7 @@ class DATAclass:
 
         df_final = pd.DataFrame(df_output)
 
-        self.actual_plot(df_final, test='Transfer')
+        self.actual_plot(df_final, test_name, test='Transfer')
         
         print(f"Arquivo: {file}, Vg: {vg:.2f} V, Vds: {vds:.2f} V, Ids: {ids:.2f} A")
 
@@ -266,8 +266,8 @@ if __name__ == "__main__":
     data = DATAclass()
 
     local_folder = "Ensaios"
-    test_name = "TRANSFER1"
+    test_name = "MOS_VDS10_"
 
     file_final = f"{local_folder}/{test_name}/{test_name}_ALL.csv"
 
-    data.actual_plot(pd.read_csv(file_final), test_name, test='Transfer')
+    data.sweep_plot(local_folder, test_name, test='Transfer')
