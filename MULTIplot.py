d1560d4386ae2c183d8394af096d544dfef25f76
Caruso Home <mateuscaruso@gmail.com>
2026-04-27 18:13:07 -0300

    .

--- MULTIplot.py
+++ MULTIplot.py
@@ -1,3 +1,12 @@
+"""Plotar as curvas de transferência e saída dos MOSFETs a partir dos arquivos CSV gerados pelo sistema:
+
+- Certifique-se de que os arquivos CSV estejam no formato correto, com colunas nomeadas adequadamente (ex: 'Vg', 'Ids', 'Vds', etc.).
+- O script irá gerar gráficos para as curvas de transferência (Ids vs Vg) e saída (Ids vs Vds) com base nos arquivos fornecidos.
+- As curvas devem estar consolidadas em um único gráfico para cada tipo de teste (Transferência e Saída), permitindo comparação direta entre as condições de teste (ex: temperatura).
+- As curvas de transferência devem ter VD no nome, definindo o valor utilizado como tensão dreno-source.
+- As curvas de saída devem ter VG no nome, definindo o valor de tensão gate-source.
+- Ajuste os parâmetros de plotagem conforme necessário para melhor visualização dos dados."""
+
 import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib.ticker as ticker
@@ -7,10 +16,11 @@ folder = 'Dados'
 
 files = [
         "M2RT_VG_final.csv",
-        "M2CT_VG_final.csv",
+        "MOSRT_VG_datasheet.csv"
         ]
 
-vgs = [15] #Caso Output, colocar os Vgs desejados aqui para filtrar as colunas [10, 15, ...]
+#vgs = [2.5, 3, 4, 6, 8, 10, 12, 15] #Caso Output, colocar os Vgs desejados aqui para filtrar as colunas [10, 15, ...]
+vgs = [15]
 
 # ========== PLOTAGEM ==========
 
@@ -22,37 +32,32 @@ if test == 'Transfer':
 
     for f in files:
         df = pd.read_csv(f"{folder}/{f}")
+        df = df.sort_values(by='Vg').reset_index(drop=True)
         
-        # Ajuste de labels e cores
         f_label = '298 K' if 'RT' in f else '77 K'
         f_color = 'red' if 'RT' in f else 'blue'
         f_adj = 0 if 'RT' in f else 0.9
         
-        # Dados atuais
         x = df['Vg']
         y = df['Ids'] + f_adj
         
         # --- LÓGICA DE AJUSTE LINEAR (REGRESSÃO) ---
-        # Selecionamos, por exemplo, apenas pontos onde Vg > 2.5V para o ajuste
-        # Você pode ajustar esse valor conforme a curva
-        mask = x > 3.5 
+
+        mask = (x >= 3.5) & (x <= 4)
         x_fit = x[mask]
         y_fit = y[mask]
         
         if len(x_fit) > 1:
+
             coef = np.polyfit(x_fit, y_fit, 1)
             a, b = coef # a é inclinação, b é intercepto em y
             poly1d_fn = np.poly1d(coef) 
-            
-            # 1. Calcula o intercepto no eixo X (onde y = 0)
             v_th_extrapolated = -b / a
             
-            # 2. Plota a reta de ajuste
-            x_range = np.linspace(v_th_extrapolated, 4.0, 100) 
+            x_range = np.linspace(v_th_extrapolated, 5.0, 100) # Plota a reta de ajuste
             plt.plot(x_range, poly1d_fn(x_range), ls='--', lw = 0.5, c=f_color, alpha=0.8)
                      
-            # 5. Adiciona um texto com o valor do intercepto (opcional)
-            plt.text(v_th_extrapolated-0.05, -4.5, f'{v_th_extrapolated:.2f}', 
+            plt.text(v_th_extrapolated-0.15, 1, f'{v_th_extrapolated:.2f}', 
                     color=f_color, fontsize='x-small', fontweight='bold')
 
         # Plot original dos pontos
@@ -71,10 +76,10 @@ if test == 'Transfer':
 
     plt.xlabel('$V_{G}$, Gate-to-Source Voltage [V]')
     plt.ylabel('$I_{D}$, Drain-to-Source Current [A]')
-    plt.xlim(2, 4)
-    plt.ylim(0, 100)
+    plt.xlim(2, 5)
+    plt.ylim(0, 150)
     plt.grid(True, linestyle='-', alpha=0.6)
-    plt.legend(loc='upper left', fontsize='small', ncol=1)
+    #plt.legend(loc='upper left', fontsize='small', ncol=1)
     plt.tight_layout()
     plt.show()
 
@@ -116,11 +121,11 @@ elif test == 'Output':
             df_plot = df_plot[df_plot[c] > 0]
             df_plot['Ron'] = df_plot['Vds'] / df_plot[c]
             
-            t_label = c.split('(')[-1].split(')')[0] + suffix
+            t_label = c.split('(')[-1].split(')')[0] #+ suffix
             t_label = t_label.replace('Vg', '$V_{G}$')
             
             ax1.plot(df_plot['Vds'], df_plot[c], ls='-', marker='.', markersize=5, 
-                    label=t_label, color=colors[i], lw=1.5)
+                    label=t_label, lw=1.5) #color=colors[i]
             
             ax2.plot(df_plot[c], df_plot['Ron']*1000,ls='-', marker='.', markersize=5, 
                     label=t_label, color=colors[i], lw=1.5)
@@ -131,19 +136,19 @@ elif test == 'Output':
                 max_ron_local = filtro_ron['Ron'].max() * 1000
                 max_ron_mili = max(max_ron_mili, max_ron_local)
 
-    # --- CONFIGURAÇÕES FINAIS: GRÁFICO 1 ---
+    # --- CONFIGURAÇÕES FINAIS: GRÁFICO OUTPUT ---
 
     plt.figure(1)
-    plt.title(f'Output Characteristics')
     plt.xlabel('$V_{DS}$, Drain-to-Source Voltage [V]')
     plt.ylabel('$I_{D}$, Drain-to-Source Current [A]')
-    #plt.xlim(0, 2.5)
-    #plt.ylim(0, 150)
+    plt.xlim(0, 4)
+    plt.ylim(0, 180)
     plt.grid(True, linestyle='-', alpha=0.6)
-    plt.legend(loc='upper right', fontsize='small')
+    plt.legend(loc='upper left', fontsize='small')
     plt.tight_layout()
 
-    # --- CONFIGURAÇÕES FINAIS: GRÁFICO 2 ---
+    # --- CONFIGURAÇÕES FINAIS: GRÁFICO Rds(on) ---
+    
     plt.figure(2)
     plt.title('On-Resistance Characteristics')
     plt.xlabel('$I_{D}$, Drain-to-Source Current [A]')
